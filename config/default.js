726c468af5ba5f5cf84227602ce27c28c7decc1a
janice143 <421827415@qq.com>
2022-07-20 16:38:06 +0800

    ignore some files

--- config/default.js
+++ config/default.js
@@ -1,9 +1,8 @@
 module.exports = {
 	port: parseInt(process.env.PORT, 10) || 3000,
-	SERVICEADDRESS:'http://localhost:3000',
-	// SERVICEADDRESS:'http://101.34.27.188:3000',
+	// SERVICEADDRESS:'http://localhost:3000', // 本地
+	SERVICEADDRESS:'http://101.34.27.188:3000', // 真实服务器
 	url: 'mongodb://localhost:27017/schoolstore',
-	// url:'mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb/schoolstore',
 	session: {
 		name: 'SID',
 		secret: 'SID',
