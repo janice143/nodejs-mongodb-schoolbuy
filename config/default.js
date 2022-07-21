d18d09ebab28eb7227c1b28ed088e7f2e80481fd
janice143 <421827415@qq.com>
2022-07-21 15:08:12 +0800

    changed

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: parseInt(process.env.PORT, 10) || 3000,
-	// SERVICEADDRESS:'http://localhost:3000', // 本地
-	SERVICEADDRESS:'http://101.34.27.188:3000', // 真实服务器
+	SERVICEADDRESS:'http://localhost:3000', // 本地
+	// SERVICEADDRESS:'http://101.34.27.188:3000', // 真实服务器
 	url: 'mongodb://localhost:27017/schoolstore',
 	session: {
 		name: 'SID',
